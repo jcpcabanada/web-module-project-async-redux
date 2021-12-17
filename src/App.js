bbd4890fb851d193c45076db05b7f630bf2aa3d3
jcpcabanada <jcpcabanada@gmail.com>
2021-12-16 20:17:02 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+
+const options = {
+    method: 'GET',
+    url: 'https://anime-collection.p.rapidapi.com/season',
+    headers: {
+        'x-rapidapi-host': 'anime-collection.p.rapidapi.com',
+        'x-rapidapi-key': '4228a4f3d0msh664b1b28f042b20p1e93fcjsn52fed2c596ec'
+    }
+};
+
+axios
+    .request(options)
+    .then(res => {
+        console.log(res.data);
+    }).catch(err => {
+    console.error(err);
+});
 
 function App() {
-  return (
-    <div className="App">
-      Async Redux Project
-    </div>
-  );
+    return (
+        <div className="App">
+            Weekly Anime Release Days
+        </div>
+    );
 }
 
 export default App;
